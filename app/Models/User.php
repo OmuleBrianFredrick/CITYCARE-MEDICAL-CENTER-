d13b33c5b92c5647e7fcad6747f9a85429b8d78f
Omule Brian Fredrick <omulebrianfredrick@gmail.com>
2026-08-19 19:23:26 +0300

    feat: connect users to employee invitations

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -36,6 +37,16 @@ class User extends Authenticatable
     public function staffProfile(): HasOne
     {
         return $this->hasOne(StaffProfile::class);
+    }
+
+    public function invitationsReceived(): HasMany
+    {
+        return $this->hasMany(EmployeeInvitation::class, 'user_id');
+    }
+
+    public function invitationsSent(): HasMany
+    {
+        return $this->hasMany(EmployeeInvitation::class, 'invited_by');
     }
 
     public function hasRole(string|array $roles): bool
