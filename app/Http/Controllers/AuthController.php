89b9f4da33f9257cf3120e798e8151e2a3241c6f
Omule Brian Fredrick <omulebrianfredrick@gmail.com>
2026-08-19 17:04:48 +0300

    fix: remove unused login dependency

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,7 +7,6 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\RateLimiter;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 use Illuminate\View\View;
 
