1e8e12949c0235e7d0468bcf930a748c8b4284d5
Omule Brian Fredrick <omulebrianfredrick@gmail.com>
2026-08-22 13:39:20 +0300

    feat: add medication formulation factory

---
+++ database/factories/MedicationFormulationFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Medication;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/** @extends Factory<\App\Models\MedicationFormulation> */
+class MedicationFormulationFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'medication_id' => Medication::factory(),
+            'strength' => fake()->randomElement(['250', '500', '5', '10']),
+            'unit' => fake()->randomElement(['mg', 'mg/ml', 'mcg']),
+            'pack_size' => fake()->randomElement(['10 tablets', '20 tablets', '100 ml']),
+            'sku' => strtoupper(fake()->unique()->bothify('SKU-######')),
+            'is_active' => true,
+        ];
+    }
+}
