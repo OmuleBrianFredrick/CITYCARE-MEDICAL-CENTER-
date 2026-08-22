badfcdd5ac0068265d35c7a418d009f084f5625b
Omule Brian Fredrick <omulebrianfredrick@gmail.com>
2026-08-23 00:33:12 +0300

    Add billing payment form request for HTTP validation

---
+++ app/Http/Requests/StoreBillingPaymentRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreBillingPaymentRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'amount' => ['required', 'numeric', 'gt:0'],
+            'method' => ['required', 'string', 'in:cash,mobile_money,bank_transfer,card,insurance,other'],
+            'transaction_reference' => ['nullable', 'string', 'max:255'],
+            'notes' => ['nullable', 'string', 'max:2000'],
+        ];
+    }
+}
