14dc592964bfd5db92fa4ede3741239f6af3adec
Omule Brian Fredrick <omulebrianfredrick@gmail.com>
2026-08-23 10:15:12 +0300

    Add CSV export action to report presentation

--- resources/views/reports/show.blade.php
+++ resources/views/reports/show.blade.php
@@ -14,6 +14,15 @@
         <div>{{ $run->error_message }}</div>
     @endif
 
+    @if ($run->status === \App\Models\ReportRun::STATUS_COMPLETED)
+        <form method="POST" action="{{ route('reports.export') }}">
+            @csrf
+            <input type="hidden" name="report_run" value="{{ $run->id }}">
+            <input type="hidden" name="format" value="csv">
+            <button type="submit">Export CSV</button>
+        </form>
+    @endif
+
     @if ($run->result_metadata)
         <pre>{{ json_encode($run->result_metadata, JSON_PRETTY_PRINT) }}</pre>
     @endif
