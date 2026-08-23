bd2629a59346e41562e173a952c86225f93cf828
Omule Brian Fredrick <omulebrianfredrick@gmail.com>
2026-08-23 05:55:01 +0300

    test: add goods receipt factory

---
+++ database/factories/GoodsReceiptFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\PurchaseOrder;
+use App\Models\InventoryStore;
+use App\Models\Facility;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class GoodsReceiptFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'facility_id' => Facility::factory(),
+            'purchase_order_id' => PurchaseOrder::factory(),
+            'store_id' => InventoryStore::factory(),
+            'received_by_id' => User::factory(),
+            'receipt_number' => 'GR-'.$this->faker->unique()->numerify('########'),
+            'status' => 'posted',
+            'received_at' => now(),
+            'notes' => null,
+        ];
+    }
+}
