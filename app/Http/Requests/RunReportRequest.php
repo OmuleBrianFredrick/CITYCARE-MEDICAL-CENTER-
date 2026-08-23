9bec0a9a9d41d9b8998c944dc9f53728d7a2a408
Omule Brian Fredrick <omulebrianfredrick@gmail.com>
2026-08-23 08:11:38 +0300

    Add reporting HTTP request validation for 13.5

---
+++ app/Http/Requests/RunReportRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RunReportRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return $this->user()?->can('reports.view') ?? false;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'facility_id' => ['nullable', 'integer', 'exists:facilities,id'],
+            'date_from' => ['nullable', 'date'],
+            'date_to' => ['nullable', 'date', 'after_or_equal:date_from'],
+            'filters' => ['nullable', 'array'],
+        ];
+    }
+
+    protected function prepareForValidation(): void
+    {
+        $filters = $this->input('filters', []);
+
+        $this->merge([
+            'filters' => array_filter([
+                ...$filters,
+                'facility_id' => $this->input('facility_id'),
+                'date_from' => $this->input('date_from'),
+                'date_to' => $this->input('date_to'),
+            ], static fn ($value) => $value !== null && $value !== ''),
+        ]);
+    }
+}
