dda6364dd51cf880b33efd1b6eff1fd553f86baa
Omule Brian Fredrick <omulebrianfredrick@gmail.com>
2026-08-23 13:11:21 +0300

    security: fail fast on discarded mass-assignment attributes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
@@ -16,6 +17,10 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        Model::preventSilentlyDiscardingAttributes(
+            $this->app->environment(['local', 'testing'])
+        );
+
         RateLimiter::for('login', function (Request $request) {
             $email = strtolower(trim((string) $request->input('email', '')));
 
